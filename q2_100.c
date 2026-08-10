f2855109b163a7e974b99b4b51a34431ed6f71f0
Kshitij Arora <kshitijarora@Kshitijs-MacBook-Air.local>
2026-08-10 22:56:36 +0530

    Add Q2 solution

---
+++ q2_100.c
@@ -0,0 +1,25 @@
+// Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("Sum = %d\n", a + b);
+    printf("Difference = %d\n", a - b);
+    printf("Product = %d\n", a * b);
+
+    if (b != 0)
+    {
+        printf("Quotient = %.2f\n", (float)a / b);
+    }
+    else
+    {
+        printf("Quotient = Undefined\n");
+    }
+
+    return 0;
+}
