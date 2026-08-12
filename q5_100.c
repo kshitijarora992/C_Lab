dafbf4701fd25bba7f04fff23503aab91644343b
Kshitij Arora <kshitijarora@Kshitijs-MacBook-Air.local>
2026-08-12 22:53:59 +0530

    Add Q5 temperature conversion program

---
+++ q5_100.c
@@ -0,0 +1,15 @@
+// Write a program to convert temperature from celsius to fahrenheit
+
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    printf("Fahrenheit=%.0f", fahrenheit);
+
+    return 0;
+}
