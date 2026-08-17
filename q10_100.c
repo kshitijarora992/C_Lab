eeff74a18e5cb0a7c16b72e3a2bb4b68ede68d62
Kshitij Arora <kshitijarora@Kshitijs-MacBook-Air.local>
2026-08-17 12:55:23 +0530

    Add Q10 time conversion program

---
+++ q10_100.c
@@ -0,0 +1,18 @@
+// Q10 (User Inputs, Operations & Output)
+// Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
+#include <stdio.h>
+
+int main() {
+    int seconds, hours, minutes, remainingSeconds;
+
+    scanf("%d", &seconds);
+
+    hours = seconds / 3600;
+    minutes = (seconds % 3600) / 60;
+    remainingSeconds = seconds % 60;
+
+    printf("%d:%d:%d", hours, minutes, remainingSeconds);
+
+    return 0;
+}
