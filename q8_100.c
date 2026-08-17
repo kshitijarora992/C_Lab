077dcfb05626f448353dd76226acb175f8ad52e1
Kshitij Arora <kshitijarora@Kshitijs-MacBook-Air.local>
2026-08-17 12:48:47 +0530

    Add Q8 sum of natural numbers

---
+++ q8_100.c
@@ -0,0 +1,18 @@
+// Q8 (User Inputs, Operations & Output)
+// Write a program to find and display the sum of the first n natural numbers.
+
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+
+    printf("Sum=%d", sum);
+
+    return 0;
+}
